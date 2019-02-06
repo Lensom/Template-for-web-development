89ee21c26584666da1557c34c8699c4dfe413d85
unknown <magurawork@gmail.com>
2019-02-06 11:07:03 +0300

    change vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp         = require('gulp'),
-	// sass           = require('gulp-sass'),
-	browserSync    = require('browser-sync'),
-	concat         = require('gulp-concat'),
-	del            = require('del'),
-	cache          = require('gulp-cache'),
-	autoprefixer   = require('gulp-autoprefixer'),
-	notify         = require('gulp-notify'),
-	stylus 				 = require('gulp-stylus');
+var gulp         = require('gulp');
+// var sass         = require('gulp-sass');
+var	browserSync    = require('browser-sync');
+var	concat         = require('gulp-concat');
+var	del            = require('del');
+var	cache          = require('gulp-cache');
+var	autoprefixer   = require('gulp-autoprefixer');
+var	notify         = require('gulp-notify');
+var	plumber				 = require('gulp-plumber');
+var	stylus 				 = require('gulp-stylus');
  
 	
 	gulp.task('browser-sync', function() {
@@ -20,12 +21,24 @@ var gulp         = require('gulp'),
 		});
 	});
 
+	gulp.task('browser-sync',()=>{
+		browserSync.init({
+				server: {
+						baseDir: `project`,
+				},          
+				notify: false,
+				open: true,
+				cache: false
+		});
+	});
+
 // User scripts
 
 gulp.task('js', gulp.series( function(){
 	return gulp.src([
 		'project/js/common.js', // Always at the end
 		])
+	// .pipe(plumber())
 	.pipe(concat('scripts.js'))
 	.pipe(gulp.dest('project/js'))
 	.pipe(browserSync.reload({ stream: true }));
@@ -34,17 +47,24 @@ gulp.task('js', gulp.series( function(){
 
 gulp.task('styl', () => {
 	return gulp.src(['project/stylus/**/*.styl']) // Take all style files
+		.pipe(plumber())
 		.pipe(stylus({ 'include css': true, })) // Take all import css and create 1 file
 		.pipe(autoprefixer(['last 15 versions']))
 		.pipe(gulp.dest(`project/css`))
-		.pipe(browserSync.stream())
+		.pipe(browserSync.reload({ stream: true }));
 });
 
+gulp.task('html', gulp.series( function(){
+	return gulp.src(['project/*.html'])
+	.pipe(plumber())
+	.pipe(browserSync.reload({ stream: true }));
+}));
 
-gulp.task('watch', gulp.series( 'styl', 'js', 'browser-sync'), function () {
-	gulp.watch('project/stylus/**/*.styl', ['styl']);
-	gulp.watch(['libs/**/*.js', 'project/js/common.js'], ['js']);
-	gulp.watch('project/*.html', browserSync.reload);
+
+gulp.task('watch', function () {
+	gulp.watch('project/stylus/**/*.styl', gulp.series('styl'));
+	gulp.watch(['libs/**/*.js', 'project/js/common.js'], gulp.series('js'));
+	gulp.watch('project/*.html', gulp.series('html'));
 });
 
 gulp.task('removedist', function() { return del.sync('dist'); });
@@ -77,4 +97,5 @@ gulp.task('build', gulp.series('removedist', 'styl', 'js'), function () {
 
 gulp.task('clearcache', function () { return cache.clearAll(); });
 
-gulp.task('default', gulp.series('watch'));
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
