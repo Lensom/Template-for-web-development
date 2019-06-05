c1b4d8367dd3139036121bfe0bf769aae6434aec
Lensom <magurawork@gmail.com>
2019-06-05 20:46:08 +0300

    add gulp for css, remove css styles and add folder links

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,58 @@
-var gulp         	 = require('gulp');
-var	browserSync    = require('browser-sync');
-var	concat         = require('gulp-concat');
-var	del            = require('del');
-var	cache          = require('gulp-cache');
-var	autoprefixer   = require('gulp-autoprefixer');
-var	plumber				 = require('gulp-plumber');
-var	stylus 				 = require('gulp-stylus');
-var cleanCSS       = require('gulp-clean-css');
-var uglify         = require('gulp-uglify');
- 
-	gulp.task('browser-sync',()=>{
-		browserSync.init({
-				server: {
-						baseDir: `project`,
-				},          
-				notify: false,
-				open: true
-		});
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var del = require('del');
+var cache = require('gulp-cache');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var stylus = require('gulp-stylus');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
+gulp.task('browser-sync', () => {
+	browserSync.init({
+		server: {
+			baseDir: `project`,
+		},
+		notify: false,
+		open: true
 	});
+});
 
 // User scripts
 
-gulp.task('js', gulp.series( function(){
+gulp.task('js', gulp.series(function () {
 	return gulp.src([
-		'project/js/jquery-3.3.1.min.js', // Add optional scripts
-		'project/js/common.js', // Always at the end
+			'project/js/jquery-3.3.1.min.js', // Add optional scripts
+			'project/js/common.js', // Always at the end
 		])
-	.pipe(plumber())
-	.pipe(concat('scripts.js'))
-	.pipe(gulp.dest('project/js'))
-	.pipe(browserSync.reload({ stream: true }));
+		.pipe(plumber())
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('project/js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 }));
 
 
 gulp.task('styl', () => {
 	return gulp.src(['project/stylus/**/main.styl']) // Take all style files
 		.pipe(plumber())
-		.pipe(stylus({ 'include css': true, })) // Take all import css and create 1 file
-		.pipe(autoprefixer(['last 3 versions']))
+		.pipe(stylus({
+			'include css': true,
+		})) // Take all import css and create 1 file
+		.pipe(autoprefixer(['last 2 versions']))
 		.pipe(gulp.dest(`project/css`))
-		.pipe(browserSync.reload({ stream: true }));
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
-gulp.task('html', gulp.series( function(){
+gulp.task('html', gulp.series(function () {
 	return gulp.src(['project/*.html'])
-	.pipe(browserSync.reload({ stream: true }));
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 }));
 
 
@@ -54,33 +62,39 @@ gulp.task('watch', function () {
 	gulp.watch('project/*.html', gulp.series('html'));
 });
 
-gulp.task('removebuild', function(cb) { del.sync('build'); cb(); });
+gulp.task('removebuild', function (cb) {
+	del.sync('build');
+	cb();
+});
 
 gulp.task('build', gulp.series('removebuild', 'styl', 'js', function (cb) {
 
 	gulp.src([
 		'project/*.html',
 		// 'project/.htaccess',
-		]).pipe(gulp.dest('build'));
+	]).pipe(gulp.dest('build'));
 
-	gulp.src(['project/css/main.css',])
-		.pipe(cleanCSS())  // Optional: min css
+	gulp.src(['project/css/*.css','project/css/main.css'
+	])
+		.pipe(cleanCSS()) // Optional: min css
 		.pipe(gulp.dest('build/css'));
 
-	gulp.src(['project/js/scripts.js',])
+	gulp.src(['project/js/scripts.js', ])
 		.pipe(uglify()) // Optional: min js
 		.pipe(gulp.dest('build/js'));
 
-	gulp.src(['project/fonts/**/*',])
+	gulp.src(['project/fonts/**/*', ])
 		.pipe(gulp.dest('build/fonts'));
 
-	gulp.src(['project/img/**/*',])
+	gulp.src(['project/img/**/*', ])
 		.pipe(gulp.dest('build/img'));
-	
+
 	cb();
-	
+
 }));
 
-gulp.task('clearcache', function () { return cache.clearAll(); });
+gulp.task('clearcache', function () {
+	return cache.clearAll();
+});
 
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
